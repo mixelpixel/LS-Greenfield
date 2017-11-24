37b7470b9be2e907ace935fb764e735fd55f4e50
mixelpixel <mixelpix@mac.com>
2017-11-23 23:09:27 -0500

    AddItem route

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
+import AddItem from './components/AddItem';
 
 ReactDOM.render(
   <Router>
     <div>
       <Route exact path='/' component={App} />
+      <Route path='/add-item' component={AddItem} />
     </div>
   </Router>,
   document.getElementById('root')
