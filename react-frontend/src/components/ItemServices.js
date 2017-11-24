f6533e1cef544d0b6d61efb0c03482bda3cd86d4
mixelpixel <mixelpix@mac.com>
2017-11-24 03:14:38 -0500

    ItemService

--- react-frontend/src/components/ItemServices.js
+++ react-frontend/src/components/ItemServices.js
@@ -1,1 +1,18 @@
 import axios from 'axios';
+
+class ItemService {
+  /* eslint no-console: 0  */
+  sendData(data) {
+    axios.post('http://localhost:4200/items/add/post', {
+      item: data,
+    })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+}
+
+export default ItemService;
