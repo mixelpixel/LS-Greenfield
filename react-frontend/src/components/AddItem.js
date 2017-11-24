81bc20c7ea6e4e26df8f35c9819a24b20de40df1
mixelpixel <mixelpix@mac.com>
2017-11-23 23:04:14 -0500

    'static' avoids the eslint error, but it seems terribly wrong. like bad... a sin.

---
+++ react-frontend/src/components/AddItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class AddItem extends Component {
+  static render() {
+    return (
+      <div>
+        <h2>Welcome to Add Item</h2>
+      </div>
+    );
+  }
+}
