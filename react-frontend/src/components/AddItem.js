143cbc5277aab55d9aa9a69c789d13154199b07b
mixelpixel <mixelpix@mac.com>
2017-11-23 23:05:10 -0500

    idem

--- react-frontend/src/components/AddItem.js
+++ react-frontend/src/components/AddItem.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 class AddItem extends Component {
+  // static seems like a bad idea. Done per: https://eslint.org/docs/rules/class-methods-use-this
   static render() {
     return (
       <div>
