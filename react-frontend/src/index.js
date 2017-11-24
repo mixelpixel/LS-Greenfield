c6c0e5741cc309f429553c9f35713dca27ab42bf
mixelpixel <mixelpix@mac.com>
2017-11-24 15:07:52 -0500

    indexing and table

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -5,13 +5,14 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import AddItem from './components/AddItem';
+import IndexItem from './components/IndexItem';
 
 ReactDOM.render(
   <Router>
     <div>
       <Route exact path='/' component={App} />
       <Route path='/add-item' component={AddItem} />
-      <Route path='/index' component={AddItem} />
+      <Route path='/index' component={IndexItem} />
     </div>
   </Router>,
   document.getElementById('root')
